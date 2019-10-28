98e402718e84afd1807a3abb36f3ac9de908642e
Okan DAVUT <davut.okan@gmail.com>
2019-10-28 13:24:52 +0300

    Listing done

--- src/App.js
+++ src/App.js
@@ -1,41 +1,71 @@
 import React, { useState, useEffect } from 'react';
 import { hot } from 'react-hot-loader/root';
+import { Card, Icon, Segment, Grid, GridColumn } from 'semantic-ui-react';
+import 'semantic-ui-css/semantic.css';
 
-import { Container, Header, Message, Segment } from "semantic-ui-react";
 
-import CardCarousel from "./CardNews";
 
-const useGithub = userName => {
+var getNewsByCategory = (type) => {
+  return fetch(`https://newsapi.org/v2/top-headlines?country=tr&category=entertainment&apiKey=bb363c3dde344f10b3f6924860f2667f`)
+    .then(r => r.json())
+};
+var getAllNews = () => {
   const [news, setNews] = useState();
   useEffect(() => {
     fetch(`https://newsapi.org/v2/top-headlines?country=tr&apiKey=bb363c3dde344f10b3f6924860f2667f`)
-      .then(r =>r.json())
-      .then(setNews);
-  }, [userName]);
-
+      .then(r => r.json())
+      .then(setNews)
+  }, []);
   return news;
 };
 
 function App() {
-  const news = useGithub('ReeganExE');
-  
-  
-  if (!news) {
-    return <div>Loading...</div>;
+  var news = getAllNews();
+
+  const listItems = (news != null && news.articles != null) ? news.articles.map((d, index) => {
+    if (d.title != null && d.title != "" && d.description != null && d.description != "")
+      return (<Card key={d.author + index}>
+        <Card.Content header={d.title.trim()} />
+        <Card.Content description={d.description.trim()} />
+        <Card.Content extra>
+          <a href={d.url} target="_blank"><Icon name='user' />Habere git</a>
+        </Card.Content>
+      </Card>
+      );
   }
-  // const listItems = news.articles.map((d) => <li key={d.publishedAt} style={{ listStyleType: "none" }}><p style={{color:"red"}}>{d.title}</p><ul ><li >{d.description}</li></ul></li>);
-
+  ) : "";
   return (
-   
-    <div>
-       <Segment attached="bottom">
-        <CardCarousel />
-      </Segment>
-    {/* {listItems } */}
-    
-    </div>
+    <div style={{ padding: "3%" }}>
+      <Grid columns={3} divided>
+        <Grid.Row>
+          <GridColumn width={2}>
+            <p>Kategoriler</p>
+            <ul>
+              <li onClick={() => (getNewsByCategory("business"))}>İş Dünyası</li>
+              <li>Eğlence</li>
+              <li>Sağlık</li>
+              <li>Bilim</li>
+              <li>Spor</li>
+              <li>Teknoloji</li>
+            </ul>
+          </GridColumn>
+          <GridColumn width={11}>
+            <Card.Group centered itemsPerRow={3}>
+              {listItems}
+            </Card.Group>
+          </GridColumn>
+          <GridColumn width={3}>
+            <p>Popüler Haberler</p>
+            <ul>
+              <li>Şahika Ercümen dünya rekoru kırdı asker selamını çaktı - Internet Haber</li>
+              <li>Böylesini evinize sakın sokmayın! Balıkta inanılmaz hile... - Hürriyet</li>
+              <li>Ekonomik krizdeki Arjantin'de sandıktan sol çıktı - Gazete Duvar</li>
+            </ul>
+          </GridColumn>
+        </Grid.Row>
+      </Grid>
+    </div >
   );
-
 }
-
 export default hot(App);
+
--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,30 @@ const options = {
   },
   module: {
     rules: [
-      { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ }
+      { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
+      {
+        test: /\.scss$/,
+        use: [{
+          loader: 'style-loader',
+        }, {
+          loader: 'css-loader',
+        }, {
+          loader: 'sass-loader',
+        }],
+      },
+      {
+        test: /\.css$/,
+        loader: 'style-loader!css-loader'
+      },
+      {
+        test: /\.s[a|c]ss$/,
+        loader: 'sass-loader!style-loader!css-loader'
+      },
+      {
+        test: /\.(jpg|png|gif|jpeg|woff|woff2|eot|ttf|svg)$/,
+        loader: 'url-loader?limit=100000'
+      }
+
     ]
   },
   plugins: [
