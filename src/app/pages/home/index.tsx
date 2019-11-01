dcae062dd3f4fa4024c9eca4b7075f44dda4a8d8
yusfgungor <yusfgungor@gmail.com>
2019-11-01 22:56:36 +0300

    The components on the main screen have been relocated

--- src/app/pages/home/index.tsx
+++ src/app/pages/home/index.tsx
@@ -1,39 +1,41 @@
 import React, { useState } from 'react';
+import styled from 'styled-components';
 import { Grid, GridColumn, GridRow } from 'semantic-ui-react';
 
 import NewsList from '../../components/NewsList';
 import CategoryMenu from '../../components/CategoryMenu';
 import CountryMenu from '../../components/CountryMenu';
 
-function Home() {
-    const [category, setCategory] = useState();
-    const [country, setCountry] = useState();
+const MenuWrapper = styled.div`
+  padding: 20px;
+`;
 
-    return (
-        <div style={{ padding: "3%" }}>
-            <Grid columns={3} divided>
-                <Grid.Row>
-                    <GridColumn width={2}>
-                        <CategoryMenu setCategory={setCategory} />
-                    </GridColumn>
-                    <GridColumn width={11}>
-                        <NewsList category={category} country={country} />
-                    </GridColumn>
-                    {/* TODO- NEWS BY COUNTRY */}
-                    <GridColumn width={3}>
-                        <CountryMenu setCountry={setCountry} />
-                    </GridColumn>
-                </Grid.Row>
-                <GridRow>
-                    <GridColumn width={3}>
-                         <a href="https://newsapi.org/" target="_blank">Api from newsapi.org</a>
-                    </GridColumn>
-                </GridRow>
-            </Grid>
-        </div >
-    );
+const Home = () => {
+	const DEFAULT_SELECTED_CATEGORY = 'all';
+	const DEFAULT_SELECTED_COUNTRY = 'tr';
+	const [category, setCategory] = useState();
+	const [country, setCountry] = useState();
+
+	return (
+		<div>
+			<MenuWrapper>
+				<CategoryMenu category={category || DEFAULT_SELECTED_CATEGORY} setCategory={setCategory} />
+				<CountryMenu country={country || DEFAULT_SELECTED_COUNTRY} setCountry={setCountry} />
+			</MenuWrapper>
+			<Grid divided>
+				<Grid.Row>
+					<GridColumn>
+						<NewsList category={category} country={country} />
+					</GridColumn>
+				</Grid.Row>
+				<GridRow>
+					<GridColumn width={3}>
+						<a href="https://newsapi.org/" target="_blank">Api from newsapi.org</a>
+					</GridColumn>
+				</GridRow>
+			</Grid>
+		</div >
+	);
 }
 
-export {
-    Home as default,
-};
+export default Home;
