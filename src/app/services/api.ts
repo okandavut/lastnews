86b851e08c84c09d8dd364680af8da3a8e21d9a5
Okan DAVUT <davut.okan@gmail.com>
2019-11-02 00:02:03 +0300

    New version deployed.

--- src/app/services/api.ts
+++ src/app/services/api.ts
@@ -1,6 +1,6 @@
 export const getNews = async (category: string, country: string = 'tr') => {
   	const categoryParam = category ? `category=${category}&` : '';
-  	const apiKey = 'bb363c3dde344f10b3f6924860f2667f'; // your api key
+  	const apiKey = ''; // your api key
 
   	return fetch(`https://newsapi.org/v2/top-headlines?${categoryParam}country=${country}&apiKey=${apiKey}`)
 		.then((response) => response.json())
