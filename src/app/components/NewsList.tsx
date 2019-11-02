e4744139e8cc2418046b2a0319afa0eed0929616
yusfgungor <yusfgungor@gmail.com>
2019-11-02 13:51:41 +0300

    added styled components to NewList Component

--- src/app/components/NewsList.tsx
+++ src/app/components/NewsList.tsx
@@ -1,26 +1,32 @@
 import React, { useState, useEffect } from 'react';
-import { Card } from 'semantic-ui-react';
+import styled from 'styled-components';
 
 import NewsBox from './NewsBox';
 import { getNews } from '../services/api';
 
+const NewsListWrapper = styled.div`
+	display: flex;
+	flex-wrap: wrap;
+  justify-content: center;
+`;
+
 function NewsList(props) {
-    const [news, setNews] = useState();
-    const newsBoxes = news && news.map((article, index) => <NewsBox article={article} key={index} />)
+	const [news, setNews] = useState();
+	const newsBoxes = news && news.map((article, index) => <NewsBox article={article} key={index} />)
 
-    useEffect(() => {
-        getNews(props.category, props.country).then(({articles}) => {
-            setNews(articles);
-        });
-    },[props.category, props.country]);
+	useEffect(() => {
+		getNews(props.category, props.country).then(({articles}) => {
+			setNews(articles);
+		});
+	},[props.category, props.country]);
 
-    
 
-    return (
-        <Card.Group centered itemsPerRow={3}>
-            {newsBoxes}
-        </Card.Group>
-    );
+
+	return (
+		<NewsListWrapper>
+			{newsBoxes}
+		</NewsListWrapper>
+	);
 }
 
 export default NewsList;
